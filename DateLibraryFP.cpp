3582e326970c607a87d81f4dace7b14645c2753f
Sidi <Sidi.com>
2026-05-25 20:22:39 +0000

    adding some formating funtions

--- DateLibraryFP.cpp
+++ DateLibraryFP.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <ctime>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 // -------------------- struct --------------------
@@ -121,6 +122,11 @@ int numberOfRemainingDays(stDate date) {
     return numberOfDaysInYear(date.year) - numberOfPassedDays(date);
 }
 
+bool isLastDayInMonth(stDate date)
+{
+    return date.day == numberOfDaysInMonth(date.month, date.year);
+}
+
 // Zeller Formula
 int getDayIndex(stDate date) {
     int d = date.day;
@@ -184,6 +190,7 @@ short daysUntilTheEndOfYear(stDate date)
         - numberOfPassedDays(date)
         - 1;
 }
+
 
 // -------------------- (2) Validation functions --------------------
 
@@ -362,6 +369,21 @@ stDate getDateByAddingMillennium(stDate date) {
     return getDateByAddingYears(date, 1000);
 }
 
+// Adds vacation business days, skipping weekends in counting.
+stDate getDateByAddingBusinessDays(stDate dateFrom, int vacationDays)
+{
+    while (vacationDays > 0)
+    {
+        dateFrom = getDateByAddingOneDay(dateFrom);
+
+        if (isBusinessDay(dateFrom))
+        {
+            vacationDays--;
+        }
+    }
+
+    return dateFrom;
+}
 
 // ------- Compare operations date -------
 
@@ -389,15 +411,35 @@ bool isDate1BeforeDate2(stDate date1, stDate date2) {
 }
 
 bool isDate1AfterDate2(stDate date1, stDate date2) {
-    if (date1.year > date2.year) return true;
-    if (date1.year < date2.year) return false;
-
-    if (date1.month > date2.month) return true;
-    if (date1.month < date2.month) return false;
-
-    return date1.day > date2.day;
+    return !isDate1BeforeDate2(date1, date2) && !isDatesEqual(date1, date2);
 }
 
+enum class enDateCompare { Before = -1, Equal = 0, After = 1 };
+enDateCompare CompareDates(stDate Date1, stDate Date2)
+{
+    if (isDate1BeforeDate2(Date1, Date2))
+        return enDateCompare::Before;
+
+    if (isDatesEqual(Date1, Date2))
+        return enDateCompare::Equal;
+
+    return enDateCompare::After;
+}
+
+short CalculateVacationDays(stDate dateFrom, stDate dateTo)
+{
+    short vacationInDays = 0;
+
+    while (isDate1BeforeDate2(dateFrom, dateTo))
+    {
+        if (!isBusinessDay(dateFrom))
+            vacationInDays++;
+
+        dateFrom = getDateByAddingOneDay(dateFrom);
+    }
+
+    return vacationInDays;
+}
 
 // ------- Subtract operations date -------
 
@@ -613,17 +655,146 @@ void printYearCalendar(int year) {
         printMonthCalendar(i, year);
 }
 
+// -------------------- (5) Periods --------------------
 
-// -------------------- (5) Main --------------------
+struct stPeriod {
+    stDate StartDate;
+    stDate EndDate;
+};
+
+stPeriod readPeriod() {
+    stPeriod period;
+
+    cout << "Enter period start date : " << endl;
+    period.StartDate = readFullDate();
+    cout << "Enter period end date : " << endl;
+    period.EndDate = readFullDate();
+
+    return period;
+}
+
+bool IsOverlapPeriods(stPeriod period1, stPeriod period2)
+{
+    return !(isDate1BeforeDate2(period1.EndDate, period2.StartDate) || isDate1BeforeDate2(period2.EndDate, period1.StartDate));
+}
+
+int PeriodLengthInDays(stPeriod Period, bool IncludeEndDate = false)
+{
+    return differenceBetweenDates(Period.StartDate, Period.EndDate, IncludeEndDate);
+}
+
+bool isDateInPeriod(stDate Date, stPeriod Period)
+{
+    return !(CompareDates(Date, Period.StartDate) == enDateCompare::Before 
+        || CompareDates(Date, Period.EndDate) == enDateCompare::After);
+}
+
+int CountOverlapDays(stPeriod Period1, stPeriod Period2)
+{
+    int Period1Length = PeriodLengthInDays(Period1, true);
+    int Period2Length = PeriodLengthInDays(Period2, true);
+    int OverlapDays = 0;
+    if (!IsOverlapPeriods(Period1, Period2))
+        return 0;
+    if (Period1Length < Period2Length)
+    {
+        while (isDate1BeforeDate2(Period1.StartDate, Period1.EndDate))
+        {
+            if (isDateInPeriod(Period1.StartDate, Period2))
+                OverlapDays++;
+            Period1.StartDate = getDateByAddingOneDay(Period1.StartDate);
+        }
+    }
+    else
+    {
+        while (isDate1BeforeDate2(Period2.StartDate, Period2.EndDate))
+        {
+            if (isDateInPeriod(Period2.StartDate, Period1))
+                OverlapDays++;
+            Period2.StartDate = getDateByAddingOneDay(Period2.StartDate);
+        }
+    }
+    return OverlapDays;
+}
+
+// -------------------- (6) Formatting --------------------
+
+vector<string> SplitString(string text, string delimiter)
+{
+    vector<string> vString;
+
+    short pos = 0;
+    string word;
+
+    while ((pos = text.find(delimiter)) != string::npos)
+    {
+        word = text.substr(0, pos);
+
+        if (word != "")
+        {
+            vString.push_back(word);
+        }
+
+        text.erase(0, pos + delimiter.length());
+    }
+
+    if (text != "")
+    {
+        vString.push_back(text);
+    }
+
+    return vString;
+}
+
+string DateToString(stDate date)
+{
+    return to_string(date.day) + "/" +
+        to_string(date.month) + "/" +
+        to_string(date.year);
+}
+
+stDate StringToDate(string dateString)
+{
+    stDate date;
+
+    vector<string> vDate = SplitString(dateString, "/");
+
+    date.day = stoi(vDate[0]);
+    date.month = stoi(vDate[1]);
+    date.year = stoi(vDate[2]);
+
+    return date;
+}
+
+string ReadStringDate(string message)
+{
+    string dateString;
+
+    cout << message;
+
+    getline(cin >> ws, dateString);
+
+    return dateString;
+}
+
+// -------------------- Main --------------------
 
 int main() {
     stDate date1 = readFullDate();
-    while (!isValidDate(date1)) {
-        cout << "Please enter a validate date : ";
-        date1 = readFullDate();
+    stDate date2 = readFullDate();
+
+    if (!isValidDate(date1) && !isValidDate(date2)) {
+        cout << "Please enter a validate dates";
     }
 
-    printDate(getDateBySubtractingDays(date1, 10));
+    enDateCompare result = CompareDates(date1, date2);
+
+    if (result == enDateCompare::Before)
+        cout << "date1 is before d2\n";
+    else if (result == enDateCompare::Equal)
+        cout << "date1 is equal to d2\n";
+    else
+        cout << "date1 is after d2\n";
 
     return 0;
 }
